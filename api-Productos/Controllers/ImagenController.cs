f4959691f04ba0db31db54b00580a150baaa0f97
Matias Zarate <m.zarate92@hotmail.com>
2025-10-16 11:23:05 -0300

    v2 armado

---
+++ api-Productos/Controllers/ImagenController.cs
@@ -0,0 +1,61 @@
+﻿using dominio;
+using negocio;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace api_Productos.Controllers
+{
+    public class ImagenController : ApiController
+    {
+        // GET: api/Imagen
+        public IEnumerable<Imagen> Get()
+        {
+            ImagenNegocio negocio = new ImagenNegocio();
+            return negocio.Listar();
+        }
+
+        // GET: api/Imagen/5
+        public Imagen Get(int id)
+        {
+            ImagenNegocio negocio = new ImagenNegocio();
+            List<Imagen> lista = negocio.Listar();
+
+            return lista.Find(x => x.ID == id);
+        }
+
+        // POST: api/Imagen
+        public void Post([FromBody]Imagen imagen)
+        {
+            ImagenNegocio negocio = new ImagenNegocio();
+            Imagen nuevo = new Imagen();
+
+            nuevo.IdArticulo = imagen.IdArticulo;
+            nuevo.ImagenUrl = imagen.ImagenUrl;
+            negocio.Agregar(nuevo);
+        }
+
+        // PUT: api/Imagen/5
+        public void Put(int id, [FromBody] Imagen imagen)
+        {
+            ImagenNegocio negocio = new ImagenNegocio();
+            Imagen modificar = new Imagen();
+
+            modificar.IdArticulo = imagen.IdArticulo;
+            modificar.ImagenUrl = imagen.ImagenUrl;
+            modificar.ID = id;
+            negocio.Modificar(modificar);
+        }
+
+        // DELETE: api/Imagen/5
+        public void Delete(int id)
+        {
+            ImagenNegocio negocio = new ImagenNegocio();
+            negocio.EliminarDB(id);
+        }
+    }
+}
---
+++ negocio/ImagenNegocio.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class ImagenNegocio
+    {
+        public List<Imagen> Listar()
+        {
+            List<Imagen> lista = new List<Imagen>();
+            AccesoBD datos = new AccesoBD();
+
+            try
+            {
+                datos.setearQuery("select Id, IdArticulo, ImagenUrl from IMAGENES");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Imagen aux = new Imagen();
+                    aux.ID = (int)datos.Lector["Id"];
+                    aux.IdArticulo = (int)datos.Lector["IdArticulo"];
+
+                    aux.ImagenUrl = (string)datos.Lector["ImagenUrl"];
+
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void Agregar(Imagen nuevo)
+        {
+            AccesoBD datos = new AccesoBD();
+            try
+            {
+                datos.setearQuery("insert into IMAGENES (IdArticulo , ImagenUrl) Values(@IdArticulo, @ImagenUrl);" + "Select cast (SCOPE_IDENTITY() AS INT) AS ID");
+                datos.setearParametro("@IdArticulo", nuevo.IdArticulo);
+                datos.setearParametro("@ImagenUrl", nuevo.ImagenUrl);
+
+                datos.ejecutarLectura();
+                if (datos.Lector.Read())
+                {
+                    nuevo.ID = Convert.ToInt32(datos.Lector["ID"]);
+                }
+                datos.cerrarLector();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void Modificar(Imagen modificar)
+        {
+            AccesoBD datos = new AccesoBD();
+            try
+            {
+                datos.setearQuery("update IMAGENES set IdArticulo = @idarticulo, ImagenUrl = @imagenurl where id = @id");
+
+                datos.setearParametro("@idarticulo", modificar.IdArticulo);
+                datos.setearParametro("@imagenurl", modificar.ImagenUrl);
+                datos.setearParametro("@id", modificar.ID);
+                datos.ejecutarAccion();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void EliminarDB(int id)
+        {
+            try
+            {
+                AccesoBD datos = new AccesoBD();
+                datos.setearQuery("Delete from IMAGENES where Id = @Id");
+                datos.setearParametro("@id", id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
